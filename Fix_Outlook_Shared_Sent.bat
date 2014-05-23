9ce709890c2aa2559daf38421fd1e3d7e9ab8502
Alec Fenichel <fenichelar@gatech.edu>
2014-05-23 11:09:30 -0400

    Add source code

---
+++ Fix_Outlook_Shared_Sent.bat
@@ -0,0 +1,13 @@
+@echo on
+
+set val=""
+for /f "tokens=2*" %%a in ('REG QUERY "HKEY_CURRENT_USER\Software\Microsoft\Office\14.0\Outlook\Preferences" /v "DelegateSentItemsStyle"') do set "val=%%~b"
+if "%val%"=="""" (
+	REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Office\14.0\Outlook\Preferences" /v "DelegateSentItemsStyle" /t "REG_DWORD" /d "1"
+)
+
+set val=""
+for /f "tokens=2*" %%a in ('REG QUERY "HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Outlook\Preferences" /v "DelegateSentItemsStyle"') do set "val=%%~b"
+if "%val%"=="""" (
+	REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Outlook\Preferences" /v "DelegateSentItemsStyle" /t "REG_DWORD" /d "1"
+)
